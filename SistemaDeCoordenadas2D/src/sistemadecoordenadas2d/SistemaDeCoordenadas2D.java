17c494ce447587e49a6ccc5d40505fd05e12f105
Santiago Frade <est.david.frade@unimilitar.edu.co>
2021-08-05 15:44:47 -0500

    Firmado por: Santiago Frade <est.david.frade@unimilitar.edu.co> Primera version de sistema de coordenadas 2D

---
+++ SistemaDeCoordenadas2D/src/sistemadecoordenadas2d/SistemaDeCoordenadas2D.java
@@ -0,0 +1,50 @@
+package sistemadecoordenadas2d;
+/* Bajo el sistema de coordenadas cartesianas referir un punto
+con sus respectivas propiedades y valores.
+dichos valores deben asignarse como una entrada por teclado
+de parte del usuario.
+*/
+public class SistemaDeCoordenadas2D 
+{
+    
+    public static void main(String[] args) 
+    {
+        Polar punto2 = new Polar();
+        Cartesiana2D punto = new Cartesiana2D();
+        /* Agregar las instrucciones necesarias para que se presente 
+        en pantalla la solicitud de los datos y posteriormente el
+        usuario los ingrese desde el teclado*/
+        punto.setX(4);
+    }
+    
+}
+ class Cartesiana2D
+{
+     private int x,y;
+     
+     public void setX (int x)
+     {
+         this.x=x;
+     }
+     public void setY (int y)
+     {
+         this.y=y;
+     }
+     
+}
+class Polar
+{
+    private float r, ang;
+
+    public void setR(float r) 
+    {
+        this.r = r;
+    }
+
+    public void setAng(float ang) 
+    {
+        this.ang = ang;
+    }
+    
+    
+}
